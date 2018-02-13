6641131249306757e3ddadd4216f8fe4f51d0785
hirosejn <hirosejn@gmail.com>
2018-02-13 23:55:06 +0900

    関数定義のimport #77

--- src/tatLogDiver/tatLogDiver-Copyright.js
+++ src/tatLogDiver/tatLogDiver-Copyright.js
@@ -1,5 +1,5 @@
 export var Copyright = {
-    Version : "b18.02.11",
+    Version : "b18.02.13",
     text : "&copy; 2017-2018 Junichiroh Hirose\n" +
                 "https://github.com/hirosejn/HJN"
 };
--- src/tatLogDiver/tatLogDiver-HJN.js
+++ src/tatLogDiver/tatLogDiver-HJN.js
@@ -1,6 +1,6 @@
 /* ******1*********2*********3*********4*********5*********6*********7****** */
 import * as Util from '../util/util.js';
-import * as TimeSeries from '../timeSeries/timeSeries.js';
+import Tat from '../timeSeries/timeSeries-Tat.js';
 
 /** @namespace */
 export var HJN = {};
@@ -8,12 +8,13 @@ window.HJN = HJN; // #67
 
 HJN.init = {}; // 初期登録処理関連
 HJN.Config = HJN.Config || Util.Config; // #76
-HJN.Tat = TimeSeries.Tat;
+HJN.Tat = Tat;
 
 HJN.chart = HJN.chartD = null;
 HJN.chartName = "chart";
 
-Util.Config("DetailGraph").setValueByKey("D_TIME", +(new Date()));  // 下段表示時刻 #27
+Util.Config("DetailGraph").setValueByKey("D_TIME", +(new Date()));  // 下段表示時刻
+                                                                    // #27
 
 
 HJN.files = [];
